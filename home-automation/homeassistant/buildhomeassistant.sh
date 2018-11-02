12c933ff97424c52db255cd531b21474cba3f41b
Kees Verhaar <kverhaar@gmail.com>
2018-11-02 16:14:32 +0100

    Updated versions

--- home-automation/homeassistant/buildhomeassistant.sh
+++ home-automation/homeassistant/buildhomeassistant.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache -t kverhaar/home-assistant-arm64v8:latest -t kverhaar/home-assistant-arm64v8:0.80.0 .
+docker build --no-cache -t kverhaar/home-assistant-arm64v8:latest -t kverhaar/home-assistant-arm64v8:0.80.3 .
